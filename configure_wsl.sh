0e5bb17d1cec5079f695a3abb0a24e66f795701b
Dennis Seidel <mail@dennisseidel.de>
2017-06-17 16:33:44 +0200

    chore: remove everything expect essntials

---
+++ configure_wsl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#install nodejs
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get -y update && sudo apt-get -y install build-essential nginx nodejs git npm yarn awscli vim ack-grep exuberant-ctags git ruby rake
+sudo npm install -g severless
+
+#update bash and vi config
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
+curl -L https://bit.ly/janus-bootstrap | bash
+#cat bash_profile_customization >> ~/.bash_profile
