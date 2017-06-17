5927a3179100266a69ed739d390f2652f11d6c73
Dennis Seidel <mail@dennisseidel.de>
2017-06-17 22:27:34 +0200

    chore: download hyper config

--- configure_wsl.sh
+++ configure_wsl.sh
@@ -10,3 +10,4 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 curl -L https://bit.ly/janus-bootstrap | bash
 #cat bash_profile_customization >> ~/.bash_profile
+curl -o .hyper.js 'https://raw.githubusercontent.com/dennisseidel/windows-development-environment/master/hyper.js'
