c4fb2b9a9cfdb4713d196870dcd1512c57829d5e
Dennis <mail@dennisseidel.de>
2017-10-28 23:52:28 +0200

    Update configure_wsl.sh

--- configure_wsl.sh
+++ configure_wsl.sh
@@ -14,3 +14,15 @@ bash-it enable alias git general
 curl -o .hyper.js 'https://raw.githubusercontent.com/dennisseidel/windows-development-environment/master/hyper.js'
 sudo apt-get install autojump
 bash-it enable plugin autojump
+
+#add support for docker -> don't forget to disable TLS in the windows UI
+# https://medium.com/software-development-stories/developing-a-dockerized-web-app-on-windows-subsystem-for-linux-wsl-61efec965080
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv \
+    --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+    --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install docker-engine
+echo "export DOCKER_HOST=tcp://127.0.0.1:2375" >> ~/.bash_profile
+sudo ln -s /mnt/c /c
