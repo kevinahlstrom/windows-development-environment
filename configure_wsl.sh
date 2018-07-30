69f8ecddef91b61edd17726ea1b174f07dffff55
Kevin Ahlstrom <kevinahlstrom@users.noreply.github.com>
2018-07-29 21:41:21 -0400

    Update docker

--- configure_wsl.sh
+++ configure_wsl.sh
@@ -29,6 +29,6 @@ sudo apt-key adv \
 	--recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
-sudo apt-get install docker-engine
+sudo apt install docker.io
 echo "export DOCKER_HOST=tcp://127.0.0.1:2375" >> ~/.bash_profile
 sudo ln -s /mnt/c /c
