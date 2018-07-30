5f6c62ab10c18facb9adc38b8a72adf10b4d0f6f
Kevin Ahlstrom <kevinahlstrom@users.noreply.github.com>
2018-07-30 04:20:51 -0700

    Update configure_wsl.sh

--- configure_wsl.sh
+++ configure_wsl.sh
@@ -6,7 +6,7 @@ sudo apt-get -y update && sudo apt-get -y install build-essential nginx nodejs g
 sudo npm install -g serverless
 
 #install terraform
-sudo apt-get install unzip
+sudo apt-get -y install unzip
 wget https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_linux_amd64.zip
 unzip terraform_0.11.1_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
@@ -18,17 +18,17 @@ bash-it enable alias git general
 #curl -L https://bit.ly/janus-bootstrap | bash
 #cat bash_profile_customization >> ~/.bash_profile
 #curl -o .hyper.js 'https://raw.githubusercontent.com/dennisseidel/windows-development-environment/master/hyper.js'
-sudo apt-get install autojump
+sudo apt-get -y install autojump
 bash-it enable plugin autojump
 
 #add support for docker -> don't forget to disable TLS in the windows UI
 # https://medium.com/software-development-stories/developing-a-dockerized-web-app-on-windows-subsystem-for-linux-wsl-61efec965080
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get -y install apt-transport-https ca-certificates
 sudo apt-key adv \
 	--keyserver hkp://ha.pool.sks-keyservers.net:80 \
 	--recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt install docker.io
+sudo apt-get -y update
+sudo apt-get -y install docker.io
 echo "export DOCKER_HOST=tcp://127.0.0.1:2375" >> ~/.bash_profile
 sudo ln -s /mnt/c /c
